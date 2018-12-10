08218bc0a95c838c599d1832d7976172a9b34870
Andreas Leicher <email@andreasleicher.com>
2018-12-10 17:17:11 +0100

    feat(backup): auth is optional, also backup with no password/user given

--- backup_mongo.sh
+++ backup_mongo.sh
@@ -21,7 +21,12 @@ date
 echo "Backing up MongoDB database ${DB}"
 
 echo "Dumping MongoDB $DB database to compressed archive"
-mongodump --authenticationDatabase ${MONGO_AUTH_DB} -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --host ${MONGO_URI} --db ${DB} --archive=$HOME/tmp_dump.gz --gzip
+if [ "$NO_AUTH" = true ]
+then
+  mongodump --host ${MONGO_URI} --db ${DB} --archive=$HOME/tmp_dump.gz --gzip
+else
+  mongodump --authenticationDatabase ${MONGO_AUTH_DB} -u ${MONGO_USERNAME} -p ${MONGO_PASSWORD} --host ${MONGO_URI} --db ${DB} --archive=$HOME/tmp_dump.gz --gzip
+fi
 
 echo "Copying compressed archive to Azure Storage: ${AZURE_SA}/${AZURE_BLOB_CONTAINER}/${DIRECTORY}/${BACKUP_NAME}"
 azcopy --source $HOME/tmp_dump.gz --destination https://${AZURE_SA}.blob.core.windows.net/${AZURE_BLOB_CONTAINER}/${DIRECTORY}/${BACKUP_NAME} --dest-key ${AZURE_DESTINATION_KEY}
